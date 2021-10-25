8c795ddecdb4ab42f8607255a96b2a5054fed572
Alshaimaa Karam <alshaimaak.cs@gmail.com>
2021-10-25 10:05:31 +0200

    Edit Html&Write function

--- script.js
+++ script.js
@@ -15,13 +15,15 @@ win = [
     [2, 4, 6]
 ]
 
+//detect and Write X or O 
 function Write(e) {
     e.target.innerText = flag;
-    if (flag == "X") {
-        flag = "O";
-    } else {
-        flag = "X"
-    }
+    flag = (flag=="X")?"O":"X";
+    // if (flag == "X") {
+    //     flag = "O";
+    // } else {
+    //     flag = "X"
+    // }
 }
 
 for (var i = 0; i < tr.length; i++) {
@@ -36,12 +38,14 @@ for (var i = 0; i < tr.length; i++) {
 //     }
 // }
 
+//Check Win
 function Win(e) {
-    // indexForTD();
-    for (var i = 0; i < win.length; i++) {
+    // var ckwin = false;
+    for (var i = 0; i < win.length || ckwin == true; i++) {
         if (td[win[i][0]].textContent === flag && td[win[i][1]].textContent === flag &&
             td[win[i][2]].textContent === flag) {
-            alert(flag + " Win")
+            alert(flag + " Win");
         }
     }
 }
+
